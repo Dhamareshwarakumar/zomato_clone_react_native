f8327db5c48b2d06b47e5ccdae6e5138e6b0dbbe
Dhamareshwarakumar <damareshwarakumar@gmail.com>
2022-10-23 17:59:06 +0530

    [frontend] Initial Setup

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+
+const App = () => {
+  return (
+    <View>
+      <Text>Welcome to Zomato</Text>
+    </View>
+  );
+};
+
+export default App;
